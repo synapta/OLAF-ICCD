e9a6074b38a55dc8c636e4f15f21e2847d252784
Davide Allavena <davide.allavena@gmail.com>
2021-01-18 10:16:17 +0100

    Added frontend needed files

---
+++ app/js/author/lombardia.js
@@ -0,0 +1,17 @@
+
+// Validate agent
+function validateAgent(el, matching = true) {
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/v1/' + params.userToken + '/validate-matching/' + encodeURIComponent(author.uri),
+        data: {
+            option: JSON.stringify(matching ? $(el).closest('.author-option').data().item : null)
+        },
+        async: true,
+        success: (data) => {
+            window.location = '/get/' + params.userToken + '/author';
+        }
+    })
+
+}
